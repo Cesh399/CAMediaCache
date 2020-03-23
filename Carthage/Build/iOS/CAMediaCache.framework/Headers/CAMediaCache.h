51ee778559e7cb2e3ad476d0d2f917192adc1713
Cesar Anez <cesaranez9@gmail.com>
2020-03-23 15:21:11 -0700

    carthage build

---
+++ Carthage/Build/iOS/CAMediaCache.framework/Headers/CAMediaCache.h
@@ -0,0 +1,27 @@
+//
+//  CAMediaCache.h
+//  CAMediaCache
+//
+//  Created by pictune on 3/23/20.
+//  Copyright © 2020 pictune. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CAMediaCache.
+FOUNDATION_EXPORT double CAMediaCacheVersionNumber;
+
+//! Project version string for CAMediaCache.
+FOUNDATION_EXPORT const unsigned char CAMediaCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAMediaCache/PublicHeader.h>
+#ifndef VIMediaCache_h
+#define VIMediaCache_h
+
+#import "VIResourceLoaderManager.h"
+#import "VICacheManager.h"
+#import "VIMediaDownloader.h"
+#import "VIContentInfo.h"
+
+#endif /* VIMediaCache_h */
+
